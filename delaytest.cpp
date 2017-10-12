2e09401be763ad53b430b1043b7707f03ea5bdbd
jonahpelfrey <jsp916@gmail.com>
2017-10-12 14:50:09 -0500

    added open connection to delay-test

--- delaytest.cpp
+++ delaytest.cpp
@@ -19,6 +19,15 @@
 
 using namespace std;
 
+int fdListenerData;
+int fdListenerCmd;
+socklen_t* pAddrlenDataIn;
+socklen_t* pAddrlenCmdIn;
+struct sockaddr* pClientAddrData;
+struct sockaddr* pClientAddrCmd;
+socklen_t* pAddrlenDataOut;
+socklen_t* pAddrlenCmdOu;
+
 timespec diff(timespec start, timespec end)
 {
 	timespec temp;
@@ -35,6 +44,107 @@ timespec diff(timespec start, timespec end)
 	}
 
 	return temp;
+}
+
+bool openConnections( 
+                      
+                      
+                      int* pFdConnectionData,
+                      int* pFdConnectionCmd )
+{
+    char addr_str[ INET6_ADDRSTRLEN ];
+    CGPPTimer   timer;
+    unsigned long millisecs;
+    socklen_t   lenDataAddr;
+    socklen_t   lenCmdAddr;
+    bool    timedout;
+    int     fddata;
+    int     fdcmd;
+    bool    retval;
+
+    retval = false;
+
+    fddata = -1;
+
+    // Copying to local variables because the addresses of the variables passed to
+    // accept() are written to; but this function is called repeatedly and must
+    // always pass the same [in] values to accept(). accept() will change the
+    // local variable, but the next call to this function will still receive
+    // the right values to give to accept().
+    memcpy( &lenCmdAddr, pAddrlenCmdIn, sizeof(socklen_t) );
+    memcpy( &lenDataAddr, pAddrlenDataIn, sizeof(socklen_t) );
+
+    // Get connection to CMD socket before continuing
+    fdcmd = accept( fdListenerCmd, pClientAddrCmd, &lenCmdAddr );
+
+    // If we got a valid file handle for the command socket
+    if( fdcmd >= 0 )
+    {
+        memcpy( pAddrlenCmdOut, &lenCmdAddr, sizeof(socklen_t) );
+
+        timedout = false;
+        timer.StartTimer();
+        while( fddata < 0 && !timedout )
+        {
+            fddata = accept( fdListenerData, pClientAddrData, &lenDataAddr );
+            if( fddata < 0 )
+            {
+                if( isRetryableError( errno ) )
+                {
+                    if( !timer.ElapsedTimeMillisecs( &millisecs ) )
+                    {
+                        timedout = true;    // timer error, exit the loop
+                    }
+                    else if( millisecs > DATA_SOCKET_CONNECT_TIMEOUT_SECS*1000 )
+                    {
+                        timedout = true;
+                    }
+                    else
+                    {
+                        usleep( 500000 );
+                    }
+                }
+                else
+                {
+                    // not really a timeout; but exit the loop
+                    timedout = true;
+                }
+            }
+            else
+            {
+                memcpy( pAddrlenDataOut, &lenDataAddr, sizeof(socklen_t) );
+            }
+        } // while( fddata == -1 && !timedout )
+        if( fddata < 0 )
+        {
+            close( fdcmd );
+        }
+    }
+
+    if( fddata >= 0 && fdcmd >= 0 )
+    {
+        *pFdConnectionData = fddata;
+        *pFdConnectionCmd = fdcmd;
+
+        // report the address of the connecting client(s)
+        inet_ntop( ((struct sockaddr_in*)pClientAddrCmd)->sin_family,
+                   (void *)&((struct sockaddr_in*)pClientAddrCmd)->sin_addr,
+                   addr_str,
+                   sizeof( addr_str ) );
+        printf( "CMD Socket accepted - %X - from - %s\n",
+                fdcmd, addr_str );
+
+        inet_ntop( ((struct sockaddr_in*)pClientAddrData)->sin_family,
+                   (void *)&((struct sockaddr_in*)pClientAddrData)->sin_addr,
+                   addr_str,
+                   sizeof( addr_str ) );
+        printf( "DAT Socket accepted - %X - from - %s\n",
+                fddata, addr_str );
+
+        retval = true;
+    }
+
+    return retval;
 }
 
 int main()
